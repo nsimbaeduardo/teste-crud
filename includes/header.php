aadc6fe36cb54e45093176efae8985a4dc1b9713
Nsimba Eduardo Matambi Lukoki <nsimba570@gmail.com>
2023-11-22 10:17:16 +0000

    h
    title

--- includes/header.php
+++ includes/header.php
@@ -2,13 +2,13 @@
 <html lang="pt-pt">
 <head>
     <meta charset="UTF-8">
-    
+
     <!--Import Google Icon Font-->
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/materialize.min.css">
     <script src="js/materialize.min.js"></script>
-    <title>Sistema de cadastro de clientes</title>
+    <title>Sistema de cadastro de cliente</title>
 </head> 
 <body>
